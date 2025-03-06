a8c0cce8de2858e637eb8d31a46fec3d9fe47dd8
JuanCarlosLuna <jcluna.tech@gmail.com>
2025-03-06 15:48:51 +0100

    Update MercadoApuestas cambios

--- MercadoApuestas/js/app3.js
+++ MercadoApuestas/js/app3.js
@@ -1,13 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sidebarTitles = document.querySelectorAll('.sidebar__title');
+document.addEventListener("DOMContentLoaded", function () {
+  let lastScrollTop = 0;
+  const header = document.querySelector(".header");
 
-    sidebarTitles.forEach(function(title) {
-      title.addEventListener('click', function() {
-        const sidebarList = title.nextElementSibling;
+  window.addEventListener("scroll", function () {
+      let scrollTop = window.scrollY;
 
-        if (sidebarList) {
-          sidebarList.classList.toggle('hidden');
-        }
-      });
-    });
+      if (scrollTop > 50) {
+          // Cuando bajo, el header se desencaja
+          header.classList.add("scrolled");
+          header.classList.remove("encajado");
+      } else {
+          // Cuando subo al top, el header se encaja de nuevo
+          header.classList.remove("scrolled");
+          header.classList.add("encajado");
+      }
+
+      lastScrollTop = scrollTop;
   });
+
+  // Animación al hacer scroll
+  const sections = document.querySelectorAll("section, .hero, .torneos, .section_banner, .section2, .section3, .banner_end, .Contactanos_rrss, .footer");
+
+  function checkScroll() {
+      const triggerBottom = window.innerHeight * 0.85;
+
+      sections.forEach(section => {
+          const sectionTop = section.getBoundingClientRect().top;
+
+          if (sectionTop < triggerBottom) {
+              section.classList.add("show");
+          }
+      });
+  }
+
+  window.addEventListener("scroll", checkScroll);
+  checkScroll(); // Para activar las secciones ya visibles al cargar
+
+  console.log("Script cargado y ejecutando checkScroll.");
+});
+
+    // MANEJO DEL CHAT IA
+    const chatIcon = document.getElementById("chatIcon");
+    const chatBox = document.getElementById("chatBox");
+    const closeChat = document.getElementById("closeChat");
+
+    chatIcon.addEventListener("click", function () {
+        chatBox.style.display = "flex"; // Muestra el chat
+    });
+
+    closeChat.addEventListener("click", function () {
+        chatBox.style.display = "none"; // Oculta el chat
+    });
